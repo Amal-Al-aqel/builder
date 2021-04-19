cec18d8dd51758cd752dc2034c1496caa8b79359
your-name <your-email@example.com>
2021-04-19 04:14:56 +0300

    Add project files.

---
+++ builder/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace builder
+{
+     class JSONBuilder
+        {
+            private string html;
+
+            public JSONBuilder()
+            {
+                this.html = "";
+            }
+
+            public JSONBuilder start()
+            {
+                this.html += "{\n";
+                return this;
+            }
+
+            public JSONBuilder setKey(string value)
+            {
+                this.html += "\"" + value + "\"" + " :";
+                return this;
+            }
+
+            public JSONBuilder setValue(string value)
+            {
+            this.html += "\"" + value + "\"" ;
+            return this;
+            }
+
+        public JSONBuilder colon()
+        {
+            this.html += ",\n";
+            return this;
+        }
+
+        public JSONBuilder end()
+            {
+                this.html += "\n}";
+                return this;
+            }
+
+            public string get()
+            {
+                return this.html;
+            }
+        }
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JSONBuilder htmlBuilder = new JSONBuilder();
+            string myString = htmlBuilder
+                             .start()
+                             .setKey("First Name")
+                             .setValue("Amal")
+                             .colon()
+                             .setKey("Last Name")
+                             .setValue("Al-Aqel")
+                             .end()
+                             .get();
+
+            Console.WriteLine(myString);
+
+        }
+    }
+
+
+}
